d5fd59ebf33446648e7203cb24d256128bc80eec
Adam Spofford <pieflavor.mc@gmail.com>
2017-05-28 21:04:41 -0700

    Add a converter from T? to Optional<T>

--- src/main/kotlin/flavor/pie/kludge/misc.kt
+++ src/main/kotlin/flavor/pie/kludge/misc.kt
@@ -3,3 +3,5 @@ package flavor.pie.kludge
 import java.util.Optional
 
 operator fun <T> Optional<T>.not(): T? = this.orElse(null)
+
+val <T: Any?> T.optional: Optional<T> get() = Optional.ofNullable(this)
