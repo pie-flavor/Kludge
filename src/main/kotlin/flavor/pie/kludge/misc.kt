cd5aacbca46309fd07f660e4fe3b4b3e56465a35
Adam Spofford <pieflavor.mc@gmail.com>
2017-05-29 16:41:18 -0700

    Add Optional<T>.unwrap()

--- src/main/kotlin/flavor/pie/kludge/misc.kt
+++
@@ -1,7 +0,0 @@
-package flavor.pie.kludge
-
-import java.util.Optional
-
-operator fun <T> Optional<T>?.not(): T? = this?.orElse(null)
-
-val <T: Any?> T.optional: Optional<T> get() = Optional.ofNullable(this)
---
+++ src/main/kotlin/flavor/pie/kludge/optionals.kt
@@ -0,0 +1,9 @@
+package flavor.pie.kludge
+
+import java.util.Optional
+
+operator fun <T> Optional<T>?.not(): T? = this.unwrap()
+
+val <T: Any?> T.optional: Optional<T> get() = Optional.ofNullable(this)
+
+fun <T> Optional<T>?.unwrap(): T? = this?.orElse(null)
