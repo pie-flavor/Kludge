301b6fb2dfdda41fcac5a605f13cf3c2c6c23f0a
Adam Spofford <pieflavor.mc@gmail.com>
2017-06-03 14:17:36 -0700

    Change the Optional of .optional to have a non-null generic type

--- src/main/kotlin/flavor/pie/kludge/optionals.kt
+++ src/main/kotlin/flavor/pie/kludge/optionals.kt
@@ -4,6 +4,6 @@ import java.util.Optional
 
 operator fun <T> Optional<T>?.not(): T? = this.unwrap()
 
-val <T: Any?> T.optional: Optional<T> get() = Optional.ofNullable(this)
+val <T: Any> T?.optional: Optional<T> get() = Optional.ofNullable(this)
 
 fun <T> Optional<T>?.unwrap(): T? = this?.orElse(null)
