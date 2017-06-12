7fd5b5d6611c1e5b2d5c4fbf1a757df359e3ba2a
Adam Spofford <pieflavor.mc@gmail.com>
2017-06-12 01:08:02 -0700

    Allow unchecked services

--- src/main/kotlin/flavor/pie/kludge/delegates.kt
+++ src/main/kotlin/flavor/pie/kludge/delegates.kt
@@ -6,8 +6,10 @@ object Service {
     inline operator fun <reified T> getValue(thisRef: Any?, property: KProperty<*>): T? {
         return !ServiceManager.provide(T::class.java)
     }
+}
 
-    inline operator fun <reified T> getValue(nothing: Nothing?, property: KProperty<*>): T? {
-        return !ServiceManager.provide(T::class.java)
+object UncheckedService {
+    inline operator fun <reified T> getValue(thisRef: Any?, property: KProperty<*>): T {
+        return ServiceManager.provideUnchecked(T::class.java)
     }
 }
