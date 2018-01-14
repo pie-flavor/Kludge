61d00532ac8137312cc9881138fbc95393a37849
Adam Spofford <pieflavor.mc@gmail.com>
2018-01-13 20:57:11 -0800

    Make delegates constructable

--- src/main/kotlin/flavor/pie/kludge/delegates.kt
+++ src/main/kotlin/flavor/pie/kludge/delegates.kt
@@ -1,15 +1,39 @@
 package flavor.pie.kludge
 
+import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 
-object Service {
-    inline operator fun <reified T> getValue(thisRef: Any?, property: KProperty<*>): T? {
-        return !ServiceManager.provide(T::class.java)
+class Service<out T : Any>(private val clazz: KClass<T>) {
+    companion object {
+        inline operator fun <reified T> getValue(thisRef: Any?, property: KProperty<*>): T? {
+            return !ServiceManager.provide(T::class.java)
+        }
+    }
+
+    private var t: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T? {
+        if (t == null) {
+            t = !ServiceManager.provide(clazz.java)
+        }
+        return t
     }
 }
 
-object UncheckedService {
-    inline operator fun <reified T> getValue(thisRef: Any?, property: KProperty<*>): T {
-        return ServiceManager.provideUnchecked(T::class.java)
+class UncheckedService<out T : Any>(private val clazz: KClass<T>) {
+    companion object {
+        inline operator fun <reified T> getValue(thisRef: Any?, property: KProperty<*>): T {
+            return ServiceManager.provideUnchecked(T::class.java)
+        }
+    }
+
+    private var t: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (t == null) {
+            t = ServiceManager.provideUnchecked(clazz.java)
+        }
+        println(property)
+        return t!!
     }
 }
