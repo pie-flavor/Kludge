0cf052b1db678275eb4e7cbfdbde5f263a05a69b
Adam Spofford <pieflavor.mc@gmail.com>
2018-01-15 15:08:39 -0800

    Convert `optional` into a function

--- src/main/kotlin/flavor/pie/kludge/optionals.kt
+++ src/main/kotlin/flavor/pie/kludge/optionals.kt
@@ -4,6 +4,6 @@ import java.util.Optional
 
 operator fun <T> Optional<T>?.not(): T? = this.unwrap()
 
-val <T: Any> T?.optional: Optional<T> get() = Optional.ofNullable(this)
+fun <T: Any> T?.optional(): Optional<T> = Optional.ofNullable(this)
 
 fun <T> Optional<T>?.unwrap(): T? = this?.orElse(null)
