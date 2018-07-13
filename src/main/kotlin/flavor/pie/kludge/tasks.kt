86e95feb91795cc17090c8e44ff919da0a9ac100
Adam Spofford <pieflavor.mc@gmail.com>
2018-07-13 00:33:13 -0700

    op-ti-mize

--- src/main/kotlin/flavor/pie/kludge/tasks.kt
+++ src/main/kotlin/flavor/pie/kludge/tasks.kt
@@ -1,11 +1,16 @@
 package flavor.pie.kludge
 
+import org.spongepowered.api.Sponge
 import org.spongepowered.api.scheduler.Task
 
-fun sync(plugin: Any, fn: () -> Unit): Task {
-    return Task.builder().execute(fn).submit(plugin)
+fun sync(plugin: Any, fn: () -> Unit) {
+    if (Sponge.isServerAvailable() && Server.isMainThread) {
+        fn()
+    } else {
+        Task.builder().execute(fn).submit(plugin)
+    }
 }
 
-fun async(plugin: Any, fn: () -> Unit): Task {
-    return Task.builder().execute(fn).submit(plugin)
+fun async(plugin: Any, fn: () -> Unit) {
+    Task.builder().async().execute(fn).submit(plugin)
 }
