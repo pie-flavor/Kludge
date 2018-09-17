eecb6d2c02070a6b4499168f2434d0262f549c8e
Adam Spofford <pieflavor.mc@gmail.com>
2018-09-16 22:46:00 -0700

    These don't actually have to be namespaced

--- src/main/kotlin/flavor/pie/kludge/coroutines.kt
+++ src/main/kotlin/flavor/pie/kludge/coroutines.kt
@@ -9,68 +9,67 @@ import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.resume
 import kotlin.coroutines.startCoroutine
 import kotlin.coroutines.suspendCoroutine
+import flavor.pie.kludge.plugin as plugin_
 
-fun task(fn: suspend TaskCoroutineContext.Companion.() -> Unit) {
-    fn.startCoroutine(TaskCoroutineContext.Companion, Continuation(EmptyCoroutineContext) {})
+fun task(fn: suspend () -> Unit) {
+    fn.startCoroutine(Continuation(EmptyCoroutineContext) {})
 }
 
 fun task(plugin: Any, fn: suspend TaskCoroutineContext.() -> Unit) {
     fn.startCoroutine(TaskCoroutineContext(plugin), Continuation(EmptyCoroutineContext) {})
 }
 
-class TaskCoroutineContext(val plugin: Any) {
+suspend fun desync(plugin: Any = plugin_) {
+    suspendCoroutine<Unit> { cont ->
+        Task.builder().async().execute { ->
+            cont.resume(Unit)
+        }.submit(plugin)
+    }
+}
 
-    companion object {
-        suspend fun desync() {
-            suspendCoroutine<Unit> { cont ->
-                Task.builder().async().execute { ->
-                    cont.resume(Unit)
-                }.submit(plugin)
-            }
-        }
-
-        suspend fun resync() {
-            if (!Server.isMainThread) {
-                suspendCoroutine<Unit> { cont ->
-                    Task.builder().execute { ->
-                        cont.resume(Unit)
-                    }.submit(plugin)
-                }
-            }
-        }
-
-        suspend fun delayTicks(ticks: Long) {
-            if (Server.isMainThread) {
-                suspendCoroutine<Unit> { cont ->
-                    Task.builder().delayTicks(ticks).execute { ->
-                        cont.resume(Unit)
-                    }.submit(plugin)
-                }
-            } else {
-                suspendCoroutine<Unit> { cont ->
-                    Task.builder().async().delayTicks(ticks).execute { ->
-                        cont.resume(Unit)
-                    }.submit(plugin)
-                }
-            }
-        }
-
-        suspend fun delay(delay: Long, unit: TimeUnit) {
-            if (Server.isMainThread) {
-                suspendCoroutine<Unit> { cont ->
-                    Task.builder().delay(delay, unit).execute { ->
-                        cont.resume(Unit)
-                    }.submit(plugin)
-                }
-            } else {
-                suspendCoroutine<Unit> { cont ->
-                    Task.builder().async().delay(delay, unit).execute { ->
-                        cont.resume(Unit)
-                    }.submit(plugin)
-                }
-            }
+suspend fun resync(plugin: Any = plugin_) {
+    if (!Server.isMainThread) {
+        suspendCoroutine<Unit> { cont ->
+            Task.builder().execute { ->
+                cont.resume(Unit)
+            }.submit(plugin)
         }
     }
+}
+
+suspend fun delayTicks(ticks: Long, plugin: Any = plugin_) {
+    if (Server.isMainThread) {
+        suspendCoroutine<Unit> { cont ->
+            Task.builder().delayTicks(ticks).execute { ->
+                cont.resume(Unit)
+            }.submit(plugin)
+        }
+    } else {
+        suspendCoroutine<Unit> { cont ->
+            Task.builder().async().delayTicks(ticks).execute { ->
+                cont.resume(Unit)
+            }.submit(plugin)
+        }
+    }
+}
+
+suspend fun delay(delay: Long, unit: TimeUnit, plugin: Any = plugin_) {
+    if (Server.isMainThread) {
+        suspendCoroutine<Unit> { cont ->
+            Task.builder().delay(delay, unit).execute { ->
+                cont.resume(Unit)
+            }.submit(plugin)
+        }
+    } else {
+        suspendCoroutine<Unit> { cont ->
+            Task.builder().async().delay(delay, unit).execute { ->
+                cont.resume(Unit)
+            }.submit(plugin)
+        }
+    }
+}
+
+class TaskCoroutineContext(private val plugin: Any) {
 
     suspend fun desync() {
         suspendCoroutine<Unit> { cont ->
