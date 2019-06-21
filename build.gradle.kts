47532b56160916b2e3d50ea6244258fb16b24402
Adam Spofford <pieflavor.mc@gmail.com>
2019-06-21 05:04:34 -0700

    Kotlin all the things

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.21"
+}
+
+group = "flavor.pie"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.spongepowered.org/maven/")
+    }
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib-jdk8"))
+    compileOnly("org.spongepowered:spongeapi:7.1.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
